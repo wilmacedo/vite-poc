7032c2f11022e37e0773fcb6f146f85b722fc406
Wil Macedo <wil.macedo.sa@gmail.com>
2022-02-09 01:28:40 -0300

    init project

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+const Navbar = () => {
+  const links = ["Features", "Solutions", "Resources", "Pricing"];
+
+  return (
+    <nav className="navbar-container">
+      <div className="navbar-links">
+        <div className="navbar-logo">AS</div>
+
+        {links.map((link, index) => (
+          <div className="navbar-item" key={index}>
+            {link}
+          </div>
+        ))}
+      </div>
+      <div className="navbar-links">
+        <div className="navbar-button-white">Log-in</div>
+        <div className="navbar-button-black">Sign up</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+import Navbar from "../../components/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
